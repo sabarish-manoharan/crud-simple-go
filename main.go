65870aacd00e923484e848b310c510feb03ce6a7
sabarish-manoharan <sabarishmm105@gmail.com>
2025-07-08 14:02:53 +0530

    refactor: restructure API to follow RESTful standards and improve response format

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -33,21 +32,16 @@ func personCreate(w http.ResponseWriter, r *http.Request) {
 
 }
 func getPerson(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		fmt.Fprintln(w, "Method Not Allowed")
+	if len(persons) == 0 {
+		fmt.Fprintln(w, "Data Not Found")
 		return
 	}
 
-	for _, p := range persons {
-		fmt.Fprintf(w, "Id : %v \t Name : %v \t Age : %v\n", p.Id, p.Name, p.Age)
-	}
+	w.Header().Set("Content-Type", "application-json")
+	json.NewEncoder(w).Encode(persons)
 
 }
 func updatePerson(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "PUT" {
-		fmt.Fprintln(w, "Method not allowed")
-		return
-	}
 	var p person
 
 	err := json.NewDecoder(r.Body).Decode(&p)
@@ -56,38 +50,46 @@ func updatePerson(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	id := p.Id
-
+	vars := mux.Vars(r)
+	found := false
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Fprintln(w, "conversion error")
+		return
+	}
 	for i, person := range persons {
 		if person.Id == id {
 			persons[i] = p
+			found = true
 		}
 	}
-	for _, person := range persons {
-		fmt.Fprintf(w, "Id : %v \t Name : %v \t Age : %v\n", person.Id, person.Name, person.Age)
+	if found {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(persons)
+	} else {
+		http.Error(w, "Element not found", http.StatusNotFound)
 	}
 
 }
 func deletePerson(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "DELETE" {
-		fmt.Fprintln(w, "Method not allowed")
-		return
-	}
-
 	vars := mux.Vars(r)
-	clean := strings.Trim(vars["id"], "{}")
-	idToRemove, err := strconv.Atoi(clean)
+	idToRemove, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		fmt.Fprintln(w, "String conversion error")
 		return
 	}
+	found := false
 	for i, p := range persons {
 		if p.Id == idToRemove {
 			persons = append(persons[:i], persons[i+1:]...)
+			found = true
 		}
 	}
-	fmt.Fprintln(w, "Deleted!")
+	if found {
+		fmt.Fprintln(w, "Deleted!")
+	} else {
+		http.Error(w, "Element Not Found", http.StatusNotFound)
+	}
 }
 func main() {
 
@@ -96,10 +98,10 @@ func main() {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world")
 	})
-	r.HandleFunc("/person", personCreate)
-	r.HandleFunc("/getPerson", getPerson)
-	r.HandleFunc("/deletePerson/{id}", deletePerson)
-	r.HandleFunc("/updatePerson", updatePerson)
+	r.HandleFunc("/person", personCreate).Methods("POST")
+	r.HandleFunc("/persons", getPerson).Methods("GET")
+	r.HandleFunc("/person/{id}", deletePerson).Methods("DELETE")
+	r.HandleFunc("/person/{id}", updatePerson).Methods("PUT")
 	http.Handle("/", r)
 	fmt.Println("Server is connected in the port 8000")
 	http.ListenAndServe(":8000", nil)
